70cfce8791bc3e1cd669d4312d6eced849cf632b
Dmitry Bezyaev <afsfhdjkvgk@gmail.com>
2024-11-03 18:22:42 +0300

    Сделано задание 1 по ООП
    Задание про банковский аккаунт

---
+++ OOP1/OOP1/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OOP1
+{
+    class BankAccount
+    {
+        private uint _balance = 0;
+
+        public uint ShowBalance()
+        { 
+            return _balance; 
+        }
+
+        public void ReplenishMoney(uint Money)
+        {
+            _balance += Money;
+        }
+
+        public uint TakeMoney(uint Money)
+        {
+            if (Money <= _balance)
+            {
+                _balance -= Money;
+                return Money;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ OOP1/OOP1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OOP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount TomBankAccount = new BankAccount();
+
+            TomBankAccount.ReplenishMoney(200);
+            Console.WriteLine(TomBankAccount.TakeMoney(33));
+            Console.WriteLine(TomBankAccount.ShowBalance());
+        }
+    }
+}
