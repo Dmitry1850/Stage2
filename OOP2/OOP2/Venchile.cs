b25f24a53e2ef1d9d68cf32967e748216a81d410
Dmitry Bezyaev <afsfhdjkvgk@gmail.com>
2024-11-03 18:33:48 +0300

    Сделано задание 2 по ООП
    Задание про базовый класс и наследование

---
+++ OOP2/OOP2/Venchile.cs
@@ -0,0 +1,45 @@
+﻿namespace OOP2
+{
+    class Venchile
+    {
+        private string _model;
+
+        public string ShowModelInfo()
+        {
+            if (_model == null)
+                return "unknown";
+
+            return _model;
+        }
+
+        public void SetModelName(string Name)
+        {
+            if (Name != null)
+                _model = Name;
+            else
+                _model = "unknown";
+        }
+    }
+
+    class Car : Venchile 
+    {
+        private int _pistons_Mass;
+
+        public int PistonsMass
+        {
+            get { return _pistons_Mass; }
+            set { _pistons_Mass = value; }
+        }
+    }
+
+    class Bicycle : Venchile
+    {
+        private bool Katafotik;
+
+        public bool KatafotikInstalled
+        {
+            get { return Katafotik; }
+            set { Katafotik = value; }
+        }
+    }
+}
