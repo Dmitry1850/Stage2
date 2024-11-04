ec7682b4eb71d5cb68c4d05d3e8f2d60377305bd
Dmitry Bezyaev <afsfhdjkvgk@gmail.com>
2024-11-04 14:33:09 +0300

    Сделано задание 4 по ООП
    Задание на абстракцию

---
+++ OOP4/OOP4/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OOP4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ OOP4/OOP4/Shape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOP4
+{
+    abstract class Shape
+    { 
+        public abstract void GetArea();
+    }
+
+    class Circle : Shape
+    {
+        public override void GetArea()
+        {
+            Console.WriteLine("Circle");
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public override void GetArea()
+        {
+            Console.WriteLine("Rectangle");
+        }
+    }
+}
