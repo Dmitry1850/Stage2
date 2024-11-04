c3ea002e1679453506ed082ffd79dd4625c59609
Dmitry Bezyaev <afsfhdjkvgk@gmail.com>
2024-11-04 14:48:22 +0300

    Решено первое задание SOLID
    Задание на SRP принцип

---
+++ SOLID1/SOLID1/Employee.cs
@@ -0,0 +1,29 @@
+﻿namespace SOLID1
+{
+    class Employee
+    {
+        public Employee(int id, string name, int salary) 
+        { 
+            _id = id;
+            _name = name;
+            _salary = salary;
+        }
+
+        private readonly int _id;
+        private readonly int _salary;
+        private readonly string _name;
+
+        public int GetID()
+        { 
+            return _id;
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+        public int GetSalary()
+        {
+            return _salary;
+        }
+    }
+}
---
+++ SOLID1/SOLID1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SOLID1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Gabben = new Employee(112, "Gabben", 1_000_000_000);
+
+            Console.WriteLine(ReturnSalary.RealSalary(Gabben));
+        }
+    }
+}
