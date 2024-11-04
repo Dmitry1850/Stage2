d93f9831631785ee9a0b1d860a9db1f959a8e6a0
Dmitry Bezyaev <afsfhdjkvgk@gmail.com>
2024-11-04 15:49:17 +0300

    Сделано задание 3 по SOLID
    Сделано задание на LSP

---
+++ SOOLID3/SOOLID3/Pinguin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SOOLID3
+{
+    class Pinguin : Bird
+    {
+        public override void Fly()
+        {
+            Console.WriteLine("Пингвин не умеет летать!");
+        }
+    }
+}
