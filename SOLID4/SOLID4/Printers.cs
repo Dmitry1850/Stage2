42c78b977b7fccdfafc36f43af770f5794ebadd3
Dmitry Bezyaev <afsfhdjkvgk@gmail.com>
2024-11-04 15:56:17 +0300

    Сделано задание 4 по SOLID
    Сделано задание на ISP

---
+++ SOLID4/SOLID4/Printers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SOLID4
+{
+    class Printer : IPrint
+    {
+        public void Print(string Document) 
+        {
+            Console.WriteLine("Я печатаю!");
+        }
+    }
+
+    class MultiPrinter :IPrintAndScan
+    {
+        public void Print(string Document)
+        {
+            Console.WriteLine("Я печатаю!");
+        }
+        public void Scan(string Document)
+        {
+            Console.WriteLine("А еще я сканирую!");
+        }
+    }
+}
