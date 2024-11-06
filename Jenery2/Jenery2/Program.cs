8922ffda31a3becbe65171cefe5df73ce9954162
Dmitry Bezyaev <afsfhdjkvgk@gmail.com>
2024-11-06 15:20:24 +0300

    Сделано задание 2 по джинерикам
    Задание на минимальное число в массиве

---
+++ Jenery2/Jenery2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Jenery2
+{
+    
+    internal class Program
+    {
+        static T FindMin<T>(T[] array) where T : IComparable<T> 
+        {
+            T min = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i].CompareTo(min) < 0)
+                {
+                    min = array[i];
+                }
+            }
+
+
+            return min;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 12, 1, 55, -1};
+
+            Console.WriteLine(FindMin(arr));
+        }
+    }
+}
