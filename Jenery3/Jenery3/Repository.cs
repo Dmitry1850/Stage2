a6312952b6948bd9f47fe15b67f6b5df0bba062b
Dmitry Bezyaev <afsfhdjkvgk@gmail.com>
2024-11-06 15:28:27 +0300

    Задание 3 джинерики
    Задание на класс с индексами

---
+++ Jenery3/Jenery3/Repository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Jenery3
+{
+    class Repository<T> where T : class
+    {
+        public Repository(int Length = 2)
+        {
+            values = new T[Length];
+        }
+
+        private T[] values;
+
+        public void SetElementOnIndex(int index, T value)
+        {
+            if (index >= 0 && index < values.Length)
+            {
+                values[index] = value;
+            }
+            else 
+            {
+                Console.WriteLine("Такого индекса нет!");
+            }
+        }
+
+        public T GetElementOnIndex(int index)
+        {
+            if (index >= 0 && index < values.Length)
+            {
+                return values[index];
+            }
+            else
+            {
+                throw new Exception("NoIndex");
+            }
+        }
+    }
+}
